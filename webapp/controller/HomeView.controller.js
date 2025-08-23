dc8282de2aa80f01dc58b44aec9017955a0d24aa
Harsh Raj <harshrajkr7261@gmail.com>
2025-08-23 13:10:30 +0000

    cond rendering of card

--- webapp/controller/HomeView.controller.js
+++ webapp/controller/HomeView.controller.js
@@ -5,6 +5,35 @@ sap.ui.define([
 
     return Controller.extend("productdetails.controller.HomeView", {
         onInit() {
+        },
+
+        onAfterRendering: function () {
+            var oFlexBox = this.byId("productFlexBox");   // FlexBox, not VBox
+            var aItems = oFlexBox.getItems();             // All VBox items
+
+            aItems.forEach(oVBox => {
+                var aCustomData = oVBox.getCustomData();
+                var oClassData = aCustomData.find(c => c.getKey() === "class");
+
+                if (oClassData) {
+                    var sClass = oClassData.getValue();
+
+                    // Remove both to avoid duplicates
+                    oVBox.removeStyleClass("card-style");
+                    oVBox.removeStyleClass("card-style1");
+
+                    // Add the bound one
+                    oVBox.addStyleClass(sClass);
+                }
+            });
+        },
+
+        onSubmit: function () {
+            sap.m.MessageToast.show("Submit pressed!");
+        },
+
+        onNavBack: function () {
+            history.go(-1);
         }
     });
 });
