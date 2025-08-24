4c4610f7b3cafe756a995226e68bba5b59e22ed1
Harsh Raj <harshrajkr7261@gmail.com>
2025-08-24 15:41:35 +0000

    made frag forloading cards

--- webapp/controller/HomeView.controller.js
+++ webapp/controller/HomeView.controller.js
@@ -1,92 +1,94 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment"
+], (Controller, JSONModel, Fragment) => {
     "use strict";
 
     return Controller.extend("productdetails.controller.HomeView", {
-        onInit() {
 
+        onInit() {
             var oModel = this.getOwnerComponent().getModel("product");
             this.__deriveDropDownData(oModel.getData().Products);
 
+            // Open filter dialog on app start
             this._openStoreDeptDialog();
-
         },
+
         __deriveDropDownData: function (aProducts) {
             var stores = [...new Set(aProducts.map(p => p.store))];
             var depts = [... new Set(aProducts.map(p => p.department))];
 
-            this.getView().setModel(new sap.ui.model.json.JSONModel({ stores: stores, departments: depts }), "dropdowns");
+            this.getView().setModel(new JSONModel({ stores: stores, departments: depts }), "dropdowns");
         },
 
-        onOpenStoreDeptDialog : function(){
+        onOpenStoreDeptDialog: function () {
             this._openStoreDeptDialog();
         },
 
         _openStoreDeptDialog: function () {
-            // Check if dialog already exists
             if (!this._oDialog) {
-                // Load dialog fragment
-                this._oDialog = sap.ui.xmlfragment(
-                    "productdetails.view.StoreDeptDialog", // path to your fragment
-                    this // controller reference
-                );
-                // Make dialog a dependent of the view to ensure proper lifecycle
-                this.getView().addDependent(this._oDialog);
+                Fragment.load({
+                    id: this.getView().getId(),
+                    name: "productdetails.view.StoreDeptDialog",
+                    controller: this
+                }).then(oDialog => {
+                    this.getView().addDependent(oDialog);
+                    this._oDialog = oDialog;
+                    this._oDialog.open();
+                });
+            } else {
+                this._oDialog.open();
             }
-
-            // Open the dialog
-            this._oDialog.open();
         },
+
+        onDialogOk: function () {
+            var store = sap.ui.getCore().byId(this.getView().getId() + "--storeSelect").getSelectedItem().getText();
+            var dept = sap.ui.getCore().byId(this.getView().getId() + "--deptSelect").getSelectedItem().getText();
+
+            var allProducts = this.getOwnerComponent().getModel("product").getData().Products;
+            var filtered = allProducts.filter(p => p.store === store && p.department === dept);
+
+            this.getView().setModel(new JSONModel({ products: filtered }), "filteredProducts");
+
+            this._oDialog.close();
+
+            // Render nested fragment with filtered data
+            this._showNestedFragment();
+        },
+
+        _showNestedFragment: function () {
+            var oContainer = this.byId("nestedCardContainer");
+            oContainer.removeAllItems();
+
+            Fragment.load({
+                id: this.getView().getId(),
+                name: "productdetails.view.NestedView",
+                controller: this
+            }).then(oFragment => {
+                oContainer.addItem(oFragment);
+
+                // Apply card color dynamically
+                var aItems = oFragment.getItems();
+                aItems.forEach(oVBox => {
+                    var aCustomData = oVBox.getCustomData();
+                    var oClassData = aCustomData.find(c => c.getKey() === "class");
+                    if (oClassData) {
+                        var sClass = oClassData.getValue();
+                        oVBox.removeStyleClass("card-style card-style1");
+                        oVBox.addStyleClass(sClass);
+                    }
+                });
+            });
+        },
+
         onDialogCancel: function () {
             this._oDialog.close();
         },
 
-        onDialogOk: function () {
-            var oView = this.getView();
-
-            // Get selected store and department
-            var store = sap.ui.getCore().byId("storeSelect").getSelectedItem().getText();
-            var dept = sap.ui.getCore().byId("deptSelect").getSelectedItem().getText();
-
-            // Get all products from manifest.json model
-            var allProducts = this.getOwnerComponent().getModel("product").getData().products;
-
-            // Filter based on selection
-            var filtered = allProducts.filter(p => p.store === store && p.department === dept);
-
-            // Set filtered data to a new model for binding
-            oView.setModel(new sap.ui.model.json.JSONModel({ products: filtered }), "filteredProducts");
-
-            this._oDialog.close();
-        },
-        onAfterRendering: function () {
-            var oFlexBox = this.byId("productFlexBox");   // FlexBox, not VBox
-            var aItems = oFlexBox.getItems();             // All VBox items
-
-            aItems.forEach(oVBox => {
-                var aCustomData = oVBox.getCustomData();
-                var oClassData = aCustomData.find(c => c.getKey() === "class");
-
-                if (oClassData) {
-                    var sClass = oClassData.getValue();
-
-                    // Remove both to avoid duplicates
-                    oVBox.removeStyleClass("card-style");
-                    oVBox.removeStyleClass("card-style1");
-
-                    // Add the bound one
-                    oVBox.addStyleClass(sClass);
-                }
-            });
-        },
-
         onSubmit: function () {
             sap.m.MessageToast.show("Submit pressed!");
-        },
-
-        onNavBack: function () {
-            history.go(-1);
         }
+
     });
 });
